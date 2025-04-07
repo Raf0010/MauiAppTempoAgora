b3e83514319142221222231f2a8fa4f1cd1ff8c2
Oi <Oi@LAPTOP-4J4A9CQR>
2025-04-07 11:57:34 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try {
+                if (!string.IsNullOrEmpty(txt_Cidade.Text)) {
+                    Tempo? t = await DataService.GetPrevisao(txt_Cidade.Text);
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
+                        dados_previsao = $"Descrição: {t.description} \n" +
+                            $"Latitude: {t.lat} \n" +
+                            $"Longitude: {t.lon} \n" +
+                            $"Temperatura máxima: {t.temp_max} \n" +
+                            $"Temperatura mínima: {t.temp_min} \n" +
+                            $"Visibilidade: {t.visibility} \n" +
+                            $"Velocidade do Vento: {t.speed} \n" +
+                            $"Nascer do Sol: {t.sunrise} \n" +
+                            $"Pôr do Sol: {t.sunset} \n";
+                        lbl_res.Text = dados_previsao;
+                    }
+                    else {
+                        lbl_res.Text = "Sem dados de previsão";
+                    }
+                } else
+                {
+                    lbl_res.Text = "Preencha a cidade";
+                }
+
+            }
+            catch (Exception ex){
+                await DisplayAlert("Algo deu errado!", ex.Message, "Okay!");
+            }
+        }
+    }
+
+}
---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,47 @@
+﻿using MauiAppTempoAgora.Models;
+using Newtonsoft.Json.Linq;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task <Tempo?> GetPrevisao(string Cidade) {
+            Tempo? t = null;
+            string chave = "0dd2f0b0aec202d4a722b2b033578b2b";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                $"q={Cidade}&units=metric&appid={chave}";
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                HttpResponseMessage resp = await httpClient.GetAsync(url);
+
+                if (resp.IsSuccessStatusCode) { 
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime time = new();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+                    t = new()
+                    {
+                        lat = (double)rascunho["coord"]["lat"],
+                        lon = (double)rascunho["coord"]["lon"],
+                        description = (string)rascunho["weather"][0]["description"],
+                        main = (string)rascunho["weather"][0]["main"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        sunrise = sunrise.ToString(),
+                        sunset = sunset.ToString(),
+                        speed = (double)rascunho["wind"]["speed"],
+                        visibility = (int)rascunho["visibility"],
+                    };
+                }
+            }
+
+
+            return t;
+                }
+    }
+}
